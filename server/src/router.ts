093b1149d9e825bf937bdcacf545f599c49e7e87
Kevin DeAlmeida <dealmeida.dak@gmail.com>
2025-06-12 10:52:26 +0200

    update get on main delete on route

--- server/src/router.ts
+++ server/src/router.ts
@@ -2,16 +2,11 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Welcome to Wild Series !");
-});
-
 // Define item-related routes
 import itemActions from "./modules/item/itemActions";
 
 router.get("/api/items", itemActions.browse);
 router.get("/api/items/:id", itemActions.read);
 router.post("/api/items", itemActions.add);
-
 
 export default router;
